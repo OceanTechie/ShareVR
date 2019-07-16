429d7af3e53a85c38f31572b3961583be76177b8
Chocobozzz <me@florianbigard.com>
2019-07-16 16:00:11 +0200

    Use chocobo.png

--- peertube-plugin-hello-world/client/common-client-plugin.js
+++ peertube-plugin-hello-world/client/common-client-plugin.js
@@ -1,10 +1,21 @@
 function register ({ registerHook, peertubeHelpers }) {
   registerHook({
     target: 'action:application.loaded',
-    handler: () => console.log('Hello application world')
+    handler: () => onApplicationLoaded(peertubeHelpers)
   })
 }
 
 export {
   register
+}
+
+function onApplicationLoaded (peertubeHelpers) {
+  console.log('Hello application world')
+
+  const baseStaticUrl = peertubeHelpers.getBaseStaticRoute()
+  const imageUrl = baseStaticUrl + '/images/chocobo.png'
+
+  const topLeftBlock = document.querySelector('.top-left-block')
+
+  topLeftBlock.style.backgroundImage = 'url(' + imageUrl + ')'
 }
