2a43a487c28b6c0a379640cc43050bfe44a62310
Chocobozzz <me@florianbigard.com>
2019-12-05 17:27:20 +0100

    Add icon content filter hook

--- peertube-plugin-hello-world/client/common-client-plugin.js
+++ peertube-plugin-hello-world/client/common-client-plugin.js
@@ -46,6 +46,17 @@ function register ({ registerHook, peertubeHelpers }) {
     handler: result => addSymbolToVideoNameResult(result, ':D')
   })
 
+  registerHook({
+    target: 'filter:internal.common.svg-icons.get-content.result',
+    handler: (result, params) => {
+      if (params.name === 'syndication') {
+        return '<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"/></svg>'
+      }
+
+      return result
+    }
+  })
+
   // Router hooks
 
   registerHook({
